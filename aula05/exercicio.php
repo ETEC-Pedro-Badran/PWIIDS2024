ac7f0292fc8e8a73b15bdb8468879ed6ad1e6dbc
Evaldo <evaldofreu@gmail.com>
2024-03-18 20:43:14 -0300

    Revisão do exercício com Strings.

--- aula05/exercicio.php
+++ aula05/exercicio.php
@@ -1,18 +1,17 @@
 <?php
 
 
+
 $str = "O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online, você pode colar o código de inserção do vídeo que deseja adicionar.";
 echo "<br>";
+// Faça um programa que conte quantos caracteres tem o texto.
 echo mb_strlen($str)."<br>";
-echo str_replace(" ","+",$str)."<br>";
-$vetor =  explode(" ", $str);
 
+// substitua todas os espaços pelo caractere +
+echo str_replace(" ","+",$str)."<br>";
+
+// transforme a string em um vetor e imprima uma palavra em cada linha
+$vetor =  explode(" ", $str);
 for($i=0;$i<count($vetor);$i++){
     echo $vetor[$i]."<br>";
 }
-
-// Faça um programa que conte quantos caracteres tem o texto.
-// substitua todas os espaços pelo caractere +
-// transforme a string em um vetor e imprima uma palavra em cada linha
-
-
